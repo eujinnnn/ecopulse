409742704df40b0b107d142a4e265c9b533773a6
decoy199 <138860145+decoy199@users.noreply.github.com>
2024-10-16 01:43:43 +0800

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -195,36 +195,39 @@ if (isset($_SESSION['confirmationMessage'])) {
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        body {
-            background-image: url('recycle2.jpg');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            margin: 0;
-            padding: 20px;
-        }
-        .container {
-            margin-top: 50px;
-        }
-        .card {
-            border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            background-color: rgba(255, 255, 255, 0.5); 
-            float: center;
-            
-        }
-        .btn-custom {
-            background-color: #28a745;
-            color: white;
-            
-        }
-        .btn-custom:hover {
-            background-color: #218838;
-        }
-        label {
-            color: #222222;
-        }
+    body {
+        background-image: url('recycle2.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        margin: 0;
+        padding: 20px;
+    }
+    .container {
+        margin-top: 50px;
+    }
+    .card {
+        border-radius: 10px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+        background-color: rgba(255, 255, 255, 0.8); 
+        float: center;
+    }
+    .btn-custom {
+        background-color: #28a745;
+        color: white;
+    }
+    .btn-custom:hover {
+        background-color: #218838;
+    }
+    label {
+        color: #222222;
+        font-size: 1.25rem;  /* Adjust the font size to make it larger */
+        font-weight: ;   /* Optionally make the text bold */
+    }
+
+
+
         /*.recycle-info {
             display: flex;
             align-items: center;
